194e02d7021e1bddd1d7467b35e86c9047f85919
Ashraf <ashrafperformance@gmail.com>
2011-08-12 17:18:11 +0300

    Tests

--- controllers/home.controller.php
+++ controllers/home.controller.php
@@ -8,18 +8,16 @@
 		protected function _action() {
 			$this->show_header();
 			
-			//$nearby_protests = $this->protest->get_nearby_protests();
-			//$this->viewer->assign('nearby_protests', $nearby_protests);
-			$results = $this->db->select('branch', 'branchid > ?', 1);
-			foreach ($results as $ball) {
-				echo $ball->branchid .': '. $ball->branchtitle;
-				echo "<BR>";
-			}
+			echo 'hello';
+
+			$this->show_footer();
+		}
+		
+		protected function test_action() {
+			$this->show_header();
 			
-			//$nearby_protests = $this->protest->FAKE_get_nearby_protests();
-			//$this->viewer->assign('nearby_protests', $nearby_protests);
-			
-			//$this->viewer->display_tpl('main');
+			echo 'test';
+
 			$this->show_footer();
 		}
 	}
